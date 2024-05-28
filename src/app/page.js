79d544b6608255000d504b0b1baff0504bfc7e0e
Caner Turna <turnacaner53@outlook.com>
2024-05-28 17:00:06 +0300

    eslint & prettier setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
+  const a = 2;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Welcome Next Js</h1>
       <Button>Click me</Button>
     </main>
