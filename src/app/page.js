387ab75f4392664208f34a543824bfcdb06784ef
Caner Turna <turnacaner53@outlook.com>
2024-05-28 17:20:54 +0300

    theme dark mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
+import ModeToggle from '@/components/ThemeModeToggle';
+
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  const a = 2;
-
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Welcome Next Js</h1>
+      <ModeToggle />
       <Button>Click me</Button>
     </main>
   );
