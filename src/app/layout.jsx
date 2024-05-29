a81c3838af8f5f678244bdd563a603b8bce192f3
Caner Turna <turnacaner53@outlook.com>
2024-05-29 23:38:09 +0300

    chore: Add React Query and React Query Devtools dependencies

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 
+import ReactQueryProvider from '@/components/ReactQueryProvider';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { Inter } from 'next/font/google';
 
-import Header from './_layout/Header';
-import Sidebar from './_layout/Sidebar';
+import MainLayout from './_layout/MainLayout';
 import './globals.css';
 import Loading from './loading';
 
@@ -19,21 +19,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange>
-          <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
-            <Sidebar />
-            <div className='flex h-screen flex-col overflow-hidden'>
-              <Header />
-              <main className='flex flex-1 flex-col gap-4 overflow-y-auto overflow-x-hidden p-4 lg:gap-6 lg:p-6'>
-                <Suspense fallback={<Loading />}>{children}</Suspense>
-              </main>
-            </div>
-          </div>
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange>
+            <MainLayout>
+              <Suspense fallback={<Loading />}>{children}</Suspense>
+            </MainLayout>
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
---
+++ src/components/ReactQueryProvider.jsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: 1000,
+      staleTime: 1000 * 0,
+    },
+  },
+});
+
+export default function ReactQueryProvider({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
